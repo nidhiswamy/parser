e828e14c3700d7a3c7e8daf904fec146dc4b35c1
nidhiswamy <nms6241@psu.edu>
2023-11-11 13:07:42 -0500

    array matching

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import numpy as np
 
 def read_files(filepath):
     try:
@@ -17,7 +18,6 @@ token_definitions = [
         ('FLOAT', r'float'),
         ('CHAR', r'char'),
         ('STRING', r'string'),
-        # ('ARR', r'((int)|(float)|(char)|(string)) [a-zA-Z_]\w*(\[\d+\])+'),
         ('ARR', r'(\[\d+\])+'),
         ('PRINT', r'print'),
         ('READ', r'read'),
@@ -29,6 +29,9 @@ token_definitions = [
         ('SEMICOLON', r';'),
         ('LPAREN', r'\('),
         ('RPAREN', r'\)'),
+        ('LBRACE', r'\{'),
+        ('RBRACE', r'\}'),
+        ('COMMA', r',\s{0,1}'),
         ('BOOLOP', r'[&]{2}|[|]{2}|!'),
         ('OPERATOR', r'[+\-*/<>=!&|]'),
     ]
@@ -94,35 +97,23 @@ def parse_datatypes(tokens, curr_token):
 
     # Identifiers
     if data_type[0] == 'ID':
-        print(f"TOKEN = {data_type}")
-        symbol_table[data_type[1]] = curr_token
-        data_type = symbol_table.get(data_type[1])
-        # One and multi-dimensional arrays 
+        print(f"Token ID = {data_type}")
+        # Variable with dimension = 0 (non-array)
+        symbol_table[data_type[1]] = (curr_token[0], 0)
+        # print(f"TOKEN = {symbol_table.get(data_type[1])}")
+        # data_type = symbol_table.get(data_type[1])
+
+        # Array handling
         if next_token[0] != 'SEMICOLON' and next_token[0] == 'ARR':
             numbers = [int(num) for num in next_token[1].strip('[]').split('][')]
-        data_type = symbol_table.get(next_token[1])
-        next_token = next(tokens)
+            symbol_table[data_type[1]] = (curr_token[0], numbers)
+            # print(f"TOKEN in symbol table = {symbol_table.get(next_token[1])}")
+            data_type = symbol_table.get(next_token[1])
+        # TODO: This is not considering next_token from line -16
+            next_token = next(tokens)
+        print(f"Next token = {next_token}")
     else:
         raise SyntaxError(f"Invalid data type: {data_type[1]}")
-
-    # Array with type[number]
-    # if curr_token[0] != 'BOOL' and data_type[0] == 'ARR':
-    #     print(f"TOKEN = {data_type}")
-    #     number = int(data_type[1].split('[')[1][:-1])
-    #     next_token = next(tokens)
-    #     if next_token[0] != 'ID':
-    #         raise SyntaxError(f"ARR: Expected an identifier, got {next_token[0]}: {next_token[1]}")
-    #     data_type = symbol_table.get(next_token[1])
-    # elif data_type[0] == 'ID':
-    #     # number = 0
-    #     symbol_table[data_type[1]] = curr_token
-    #     print(f"declared {data_type[1]} for {data_type[0]}")
-    #     data_type = symbol_table.get(data_type[1])
-    # else:
-    #     raise SyntaxError(f"Invalid data type: {data_type[1]}")
-
-    # if identifier[0] != 'ID':
-    #     raise SyntaxError(f"ID: Expected an identifier, got {identifier[0]}: {identifier[1]}")
 
     symbol_table[next_token[1]] = data_type
 
@@ -130,17 +121,10 @@ def parse_datatypes(tokens, curr_token):
         raise SyntaxError("Declaration and assignment at the same time.")
 
     if next_token[0] != 'SEMICOLON':
-        print(f"TOKEN = {next_token}")
-        raise SyntaxError(f"Declarations need to end with semicolons (;)")
+        print(f"TOKEN = {next_token[0]}")
+        raise SyntaxError(f"Declarations need to end with a semicolon (;)")
 
     symbol_table[next_token[1]] = data_type
-
-# def parse_arrays(tokens):
-#     identifier = next(tokens)
-#     number = int(identifier[1].split('[')[1][:-1])
-#     next_token = next(tokens)
-#     return
-#
 
 # Parsing all assignments (eg. a = (expression);)
 def parse_assignment(tokens, curr_token):
@@ -155,21 +139,21 @@ def parse_assignment(tokens, curr_token):
     if next_token[0] != 'ASSIGN':
         raise SyntaxError(f"Expected '='")
 
-    parse_expression(tokens, declared_type)
+    parse_expression(tokens, curr_token)
 
 # Parsing all expressions (eg. a = 4 * 5;)
-def parse_expression(tokens, declared_type):
+def parse_expression(tokens, curr_token):
     next_token = next(tokens)
-
     assigned_type = symbol_table.get(next_token[1])
+    declared_type = symbol_table.get(curr_token[1])
 
     # Implementing type checking
-    if (next_token[0] != 'NUMBER' and next_token[0] != 'TRUE' and next_token[0] != 'FALSE') and declared_type != assigned_type:
+    if (next_token[0] != 'NUMBER' and next_token[0] != 'TRUE' and next_token[0] != 'FALSE' and next_token[0] != 'LBRACE') and declared_type != assigned_type:
         print(f"Token = {next_token}")
         raise SyntaxError(f"Inconsistent type assignments of {declared_type} and {assigned_type}")
 
     # Boolean expression
-    if next_token[0] == 'TRUE' or next_token[0] == 'FALSE':
+    elif next_token[0] == 'TRUE' or next_token[0] == 'FALSE':
         print(f"Token here = {next_token}")
         operator = next(tokens)
         print(f"Operator = {operator}")
@@ -180,13 +164,54 @@ def parse_expression(tokens, declared_type):
         print(f"OP2 = {op2}")
         if op2[0] != 'TRUE' and op2[0] != 'FALSE':
             raise SyntaxError(f"Inconsistent operations of types {next_token} and {op2}")
-    
+
+    # Array initialization
+    elif next_token[0] == 'LBRACE':
+        next_token = next(tokens)
+        # array_size = array_table[next_token[0]]
+        value = symbol_table.get(curr_token[1])
+        if value is None:
+            raise SyntaxError("No such value initialized")
+        print(f"Current token's symbol = {value[1]}")
+        array_type = value[0]
+        dimension = value[1]
+        print(f"New array = {dimension}")
+        array_size = len(dimension)
+        print(f"Array size = {array_size}")
+
+        # Iterating through array sizes
+        for i in range(array_size):
+            # Iterating through the dimensions
+            next_token = next(tokens)
+            print(f"Next token = {next_token}")
+            if next_token[0] != 'LBRACE' and next_token[0] != 'NUMBER':
+                raise SyntaxError("Expected {.")
+            for num in dimension:
+                next_token = next(tokens)
+                print(f"Next token type = {next_token[0]}")
+                print(f"Array type = {array_type}")
+                if next_token[0] != array_type:
+                    raise SyntaxError(f"Inconsistent type assignments for {array_type} array")
+
+                # Comma
+                next_token = next(tokens) 
+                print(f"Next token = {next_token}")
+                if next_token[0] != 'COMMA':
+                   raise SyntaxError(f"Array dimensions do not match")
+            next_token = next(tokens)
+            print(f"Next token = {next_token}")
+            if next_token[0] != 'RBRACE':
+                raise SyntaxError("Array dimensions do not match")
+            next_token = next(tokens)
+            print(f"Next token = {next_token}")
+            if next_token[0] != 'COMMA':
+                raise SyntaxError(f"Dimension of array = {array_size}")
+
     # Type-checking assignment of two variables
     elif next_token[0] == 'ID':
         print(f"Token here = {next_token}")
         if declared_type != assigned_type:
             raise SyntaxError(f"Inconsistent type assignments of {declared_type} and {assigned_type}")
-
 
     # If expression uses an operator and operands
     elif next_token[0] == 'NUMBER':
@@ -200,10 +225,11 @@ def parse_expression(tokens, declared_type):
         if op2[0] != 'NUMBER':
             raise SyntaxError(f"Invalid second operand {op2[0]}")
 
+    # Statements have to end with a semicolon
     next_token = next(tokens)
     if next_token[0] != 'SEMICOLON':
         print(f"TOKEN at semicolon = {next_token}")
-        raise SyntaxError(f"Statements need to end with semicolons (;)")
+        raise SyntaxError(f"Statements need to end with a semicolon (;)")
 
 
 # Parsing the print(a); statement
@@ -263,6 +289,7 @@ def main(filepath):
     code = read_files(filepath)
     tokens = tokenize(code)
     parse(tokens)
+    print(f"SYMBOL TABLE = {symbol_table}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
